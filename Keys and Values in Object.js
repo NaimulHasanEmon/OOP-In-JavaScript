53a6834274ac5bf10f216cc7cb9502cf7fdec64c
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2024-02-05 02:58:47 +0600

    Add files via upload

---
+++ Keys and Values in Object.js
@@ -0,0 +1,16 @@
+let person = {
+    name: "Emon",
+    age: 23,
+    profession: 'Developer',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    salary: 30000,
+    isMarried: true,
+}
+
+// To see the keys of the object
+let keys = Object.keys(person);
+console.log(keys);
+
+// To see the values of the object
+let values = Object.values(person);
+console.log(values);
