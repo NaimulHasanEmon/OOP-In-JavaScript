c64633d3e159834bcbaf5444c8e1080dfd0852b9
Md. Naimul Hasan <120248820+emon12700@users.noreply.github.com>
2024-02-05 02:32:51 +0600

    Add files via upload

---
+++ Objects.js
@@ -0,0 +1,18 @@
+// Non-Primitive types
+
+let bottle = {
+    brand: 'Apple',
+    price : 30,
+    color : 'White',
+    isClean : true,
+}
+
+let subject = {
+    name: 'OOP',
+    teacher: 'Lameya',
+    examDate: '30 February',
+    exams: {
+        name: 'Final exam',
+        marks: 100,
+    }
+}
